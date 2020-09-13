48b5dac146adaf6f8bde7ded6c2995233c4a8734
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-09-13 09:31:14 -0400

    Set up the CRU actions for the Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: {data: users}
+  end
+
+  def show
+    user = User.find(params[:id])
+      render json: {data: user}
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save 
+      render json: {data: user}
+    else
+      render json: {message: 'user not created successfully'}
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: {data: user}
+    else
+      render json: {message: 'update not successful'}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+end
