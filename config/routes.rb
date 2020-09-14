d3d30875868e63a0223af6278b9efe6707d15fcc
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-09-14 08:31:17 -0400

    Encode the user credentials with json_web_token

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :appointments, only: [:index, :create]
   resources :cars, only: [:index, :show]
+  # get '/signup', to: 'authentication#create'
+  get 'authentication/create'
 end
