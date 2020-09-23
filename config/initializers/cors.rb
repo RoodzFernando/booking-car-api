38ed1ed09e5d9a59e3b69a1ac7800886c3eead4f
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-09-23 14:31:49 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,7 +14,7 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
-Rails.application.config.hosts << "localhost:3001"
+Rails.application.config.hosts << "pure-badlands-43483.herokuapp.com"
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
